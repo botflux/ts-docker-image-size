54c4dc6f0b525e84e7b0206e936062c6fa69bf99
Victor Mendele <v-mendele@logitud.fr>
2022-02-04 09:36:27 +0100

    try to check if layer increses image size

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,11 @@
-docker build --no-cache -f Dockerfile.double-install-and-two-containers -t tdis:double-install-and-two-containers .
-docker build --no-cache -f Dockerfile.prune-and-two-containers -t tdis:prune-and-two-containers .
-docker build --no-cache -f Dockerfile.double-install-and-single-container -t tdis:double-install-and-single-container .
-docker build --no-cache -f Dockerfile.prune-and-single-container -t tdis:prune-and-single-container .
-docker build --no-cache -f Dockerfile.double-install-and-single-container-and-cache-cleared -t tdis:double-install-and-single-container-and-cache-cleared .
-docker build --no-cache -f Dockerfile.prune-and-single-container-and-cache-cleared -t tdis:prune-and-single-container-and-cache-cleared .
+docker build -f Dockerfile.double-install-and-two-containers -t tdis:double-install-and-two-containers .
+docker build -f Dockerfile.prune-and-two-containers -t tdis:prune-and-two-containers .
+docker build -f Dockerfile.double-install-and-single-container -t tdis:double-install-and-single-container .
+docker build -f Dockerfile.prune-and-single-container -t tdis:prune-and-single-container .
+docker build -f Dockerfile.double-install-and-single-container-and-cache-cleared -t tdis:double-install-and-single-container-and-cache-cleared .
+docker build -f Dockerfile.prune-and-single-container-and-cache-cleared -t tdis:prune-and-single-container-and-cache-cleared .
+
+docker build -f Dockerfile.multiple-layer-container -t tdis:multiple-layer-container .
+docker build -f Dockerfile.one-layer-container -t tdis:one-layer-container .
 
 docker image ls | grep "tdis"
